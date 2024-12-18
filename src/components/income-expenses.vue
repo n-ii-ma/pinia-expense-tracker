57a1eb55f5cea2ff646b7ab617170fecc558b4bc
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-12-18 16:30:58 +0330

    Break long words

--- src/components/income-expenses.vue
+++ src/components/income-expenses.vue
@@ -9,13 +9,13 @@ const store = useTransactionsStore();
   <section class="grid grid-cols-2 shadow-md bg-white rounded-md p-5 my-5">
     <div class="text-center border-r border-gray-200 pr-4">
       <h4 class="font-semibold">INCOME</h4>
-      <p class="my-1 tracking-wider text-xl text-green-600 font-medium">
+      <p class="my-1 tracking-wider text-xl text-green-600 font-medium break-words">
         ${{ store.income.toFixed(2) }}
       </p>
     </div>
     <div class="text-center pl-4">
       <h4 class="font-semibold">EXPENSE</h4>
-      <p class="my-1 tracking-wider text-xl text-red-600 font-medium">
+      <p class="my-1 tracking-wider text-xl text-red-600 font-medium break-words">
         ${{ Math.abs(store.expenses).toFixed(2) }}
       </p>
     </div>
