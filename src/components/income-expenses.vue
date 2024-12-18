aa14bec5cc54e77fea1590bb6f4a4ecb6098f67c
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-12-18 13:30:47 +0330

    Show overall income and expenses

--- src/components/income-expenses.vue
+++ src/components/income-expenses.vue
@@ -1,14 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useTransactionsStore } from "@/stores/transactions";
+
+// Transactions store
+const store = useTransactionsStore();
+</script>
 
 <template>
   <div class="flex justify-between shadow-md bg-white rounded-md p-5 my-5">
-    <div class="flex-1 border-r-[1px] border-gray-200 text-center">
+    <div class="flex-1 border-r border-gray-200 text-center">
       <h4 class="font-semibold">INCOME</h4>
-      <p class="my-1 tracking-wider text-xl text-green-600 font-medium">$2000</p>
+      <p class="my-1 tracking-wider text-xl text-green-600 font-medium">
+        ${{ store.income.toFixed(2) }}
+      </p>
     </div>
     <div class="flex-1 text-center">
       <h4 class="font-semibold">EXPENSE</h4>
-      <p class="my-1 tracking-wider text-xl text-red-600 font-medium">$1000</p>
+      <p class="my-1 tracking-wider text-xl text-red-600 font-medium">
+        ${{ Math.abs(store.expenses).toFixed(2) }}
+      </p>
     </div>
   </div>
 </template>
